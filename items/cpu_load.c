efc9469a3e04ceab1642b265842b90d94800aef9
Mihai Coman <mihai.cmn@gmail.com>
2016-11-27 03:26:33 +0200

    fetch cpu load

---
+++ items/cpu_load.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+#include "routine.h"
+
+void cpu_load_routine(cfg_t *config, struct text_t *text) {
+	double loadavg[3];
+	if (getloadavg(loadavg, 3) != 3)
+		die("could not get loadavg\n");
+	text_printf(text, "%.2f %.2f %.2f", loadavg[0], loadavg[1], loadavg[2]);
+}
